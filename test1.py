70cc2a4a8b24ff27ba31c114d4836c07d6511b0f
Jerry <qitian41322@gmail.com>
2020-12-21 22:00:02 -0500

    fourth lesson

--- test1.py
+++ test1.py
@@ -1,18 +1,35 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route("/<userName>")
-def home(userName):
+@app.route("/")
+def home():
     return render_template("index.html")
 
-@app.route("/test")
-def test():
-    return render_template("test.html")
+@app.route("/login/", methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        username = request.form["nm"] # get data 
+        return redirect(url_for("user", usr=username))
+    else:
+        return render_template("login.html")
+    
+
+@app.route("/<usr>")
+def user(usr):
+    return f"<h1>{usr}<h1>"
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+
+
+
 
 
 ### Lesson 1: Introduction to Flask
@@ -25,3 +42,13 @@ if __name__ == "__main__":
 #     return redirect(url_for("city", cityName="wuhan"))
 ### Lesson 1
 
+
+
+
+
+
+### Lesson 3: Templates
+# @app.route("/test")
+# def test():
+#     return render_template("test.html")
+### Lesson 3
